d18f792004fe4003823bcf2f8694243ac63fbb26
Jana Chaloupková <janachaloupkova97@gmail.com>
2021-12-27 17:42:08 +0100

    ✨ Add movement to multiple obstacles

--- src/games/RunAndJump/obstacles/index.tsx
+++ src/games/RunAndJump/obstacles/index.tsx
@@ -1,26 +1,41 @@
-import { FC, useEffect, useRef } from 'react'
+import { FC, useCallback, useEffect, useRef } from 'react'
 import './index.css'
 
 const movingTime = 3000 // milliseconds
 const animationType = 'linear'
 
-const Obstacles: FC<{ age: number }> = ({ age }) => {
-    const obstacleRef = useRef<HTMLDivElement>(null)
-    
-    useEffect(() => {
-        const obstacle = obstacleRef.current
-        if (!obstacle) return
+const maxIntevalBetweenObstacles = 1000 // milliseconds
 
-        obstacle.style.transition = `right ${movingTime}ms ${animationType}`
-        obstacle.style.right = '100%'
-    }, [obstacleRef])
+const Obstacles: FC<{ age: number }> = ({ age }) => {
+    const obstacleRefs = useRef<Array<HTMLDivElement | null>>([])
+
+    const moveNextObstacle = useCallback((obstaclesIterator: IterableIterator<HTMLDivElement | null>) => {
+        const nextObstacle = obstaclesIterator.next()
+        if (nextObstacle.done) return // last obstacle
+
+        const obstacle = nextObstacle.value
+
+        if (obstacle) {
+            obstacle.style.transition = `right ${movingTime}ms ${animationType}`
+            obstacle.style.right = '100%'
+        }
+
+        setTimeout(() => moveNextObstacle(obstaclesIterator), maxIntevalBetweenObstacles)
+    }, [])
+
+    useEffect(() => moveNextObstacle(obstacleRefs.current.values()), [moveNextObstacle])
 
     const years = Array(age).fill(null).map((_, index) => index + 1)
 
     return (
         <>
             {years.map((year) => 
-                <div key={year} ref={obstacleRef} className='obstacle'>{year}</div>
+                <div 
+                    key={year}
+                    ref={(element) => obstacleRefs.current.push(element)}
+                    className='obstacle'>
+                        {year}
+                </div>
             )}
         </>
     )
