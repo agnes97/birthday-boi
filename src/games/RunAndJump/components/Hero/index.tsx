d4584aadf36823027cf522d8ef93dc50d8c5f5ef
Jana Chaloupková <janachaloupkova97@gmail.com>
2021-12-29 19:18:59 +0100

    ♻️ Refactor Hero component

--- src/games/RunAndJump/components/Hero/index.tsx
+++ src/games/RunAndJump/components/Hero/index.tsx
@@ -1,32 +1,17 @@
-import { FC, RefObject } from 'react'
+import { FC, RefObject, useState } from 'react'
 import './index.css'
-
-import heroHenry from '../../../../assets/henry.gif'
-import heroAgnes from '../../../../assets/agnes.gif'
 
 type Props = {
     heroRef: RefObject<HTMLDivElement>  
 }
 
 const Hero: FC<Props> = ({ heroRef }) => {
+    const [hero, setHero] = useState('henry')
+
     // TODO: Add "change hero" button to starting screen!
-    const handleClick = () => {
-        const hero = heroRef.current
+    const handleClick = () => setHero(hero === 'agnes' ? 'henry' : 'agnes')
 
-        if (!hero) return
-
-        if (hero.className === `hero agnes`) {
-            hero.style.backgroundImage = `url(${heroHenry})`
-            hero.className = 'hero henry'
-        } else {
-            hero.style.backgroundImage = `url(${heroAgnes})`
-            hero.className = 'hero agnes'
-        }
-    }
-
-    return (
-        <div ref={heroRef} className='hero henry' onClick={handleClick} />
-    )
+    return <div ref={heroRef} className={`hero ${hero}`} onClick={handleClick} />
 }
 
 export default Hero
