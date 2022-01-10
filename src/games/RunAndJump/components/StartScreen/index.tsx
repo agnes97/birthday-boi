f4a292bfdd206a76de59dbe6e542400b2fda790b
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-01-10 17:03:36 +0100

    ✨ Add Hero option to StartScreen

--- src/games/RunAndJump/components/StartScreen/index.tsx
+++ src/games/RunAndJump/components/StartScreen/index.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import './index.css'
+import Agnes from '../../../../assets/agnes.gif'
+import Henry from '../../../../assets/henry.gif'
 
 type Props = {
     startGame: () => void
@@ -10,7 +12,15 @@ const title = '{ RUN & JUMP }'
 const StartScreen: FC<Props> = ({ startGame }) => (
     <div className='start-screen-container'>
         <h2>{title}</h2>
-        <button onClick={startGame} title="PLAY GAME">&#x27A4;</button>
+        <div className='options-container'>
+            <div className='option'>
+                <img src={Henry} alt="Henry" />
+                <hr />
+                <img src={Agnes} alt="Agnes" />
+            </div>
+            <button onClick={startGame} title="PLAY GAME">&#x27A4;</button>
+            <div className='option'>OPTIONS 2</div>
+        </div>
     </div>
 )
 
